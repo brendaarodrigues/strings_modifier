07de7983a27814fb009c2bf45b0136107651b6e4
Brenda <105011689+brendaarodrigues@users.noreply.github.com>
2022-05-24 20:25:19 -0300

    Update verify.js

--- src/verify.js
+++ src/verify.js
@@ -1,7 +1,7 @@
 var charArray = ["ab", "ac", "ba", "bc", "ca", "cb"];
 
 function verifyString(string) {
-  return charArray.some((element) => string.includes(element));
+  return charArray.some((element) => string.includes(element))
 }
 
-export default verifyString;
+export default verifyString
