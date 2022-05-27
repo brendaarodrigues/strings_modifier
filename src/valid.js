e77438711cdc9fe30aff3f59c8e7377c0a240036
Brenda <105011689+brendaarodrigues@users.noreply.github.com>
2022-05-27 10:18:33 -0300

    Update valid.js

--- src/valid.js
+++ src/valid.js
@@ -1,7 +1,3 @@
-// a partir do regex que pega apenas letras que não sejam a b e c,
-// o validatestring usa o search para procurar nesse regex se tem algo que NÃO sejam as letras a b ou c
-// se tiver só A, B e C, retorna -1 (posição do que tá procurando), ou seja, true > a string possui apenas as letras permitidas
-
 function validateString(string, regex) {
   if (string.search(regex) === -1) return true;
   else return false;
